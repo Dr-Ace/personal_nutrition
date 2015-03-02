4e3acc6d15df66a43591b2225be662fd0daf0452
Robert Boyle <betawaves@gmail.com>
2015-03-01 16:04:19 -0800

    hands......

--- js/profile_charts.js
+++ js/profile_charts.js
@@ -6,8 +6,7 @@
 
 //-------------------------------------------
 
-var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
-var lineChartData2 = {
+var ironData = {
   labels : ["January","February","March","April","May","June","July"],
   datasets : [
     {
@@ -18,10 +17,24 @@ var lineChartData2 = {
       pointStrokeColor : "#fff",
       pointHighlightFill : "#fff",
       pointHighlightStroke : "rgba(220,220,220,1)",
-      data : [.09,null,.04,null,.02,.07,.03]
+      data : [10.5,null,null,11,null,null,12.5]
     }
   ]
 }
+
+var ctx = $("#canvas-iron")[0].getContext("2d");
+new Chart(ctx, {width: 375, height: 187})
+.Line(ironData, {
+  segmentShowStroke : true,
+  scaleShowValues: true,
+  scaleValuePaddingX: 13,
+  scaleValuePaddingY: 13,
+  bezierCurve : false,
+  scaleOverride: true,
+  scaleSteps: 7,
+  scaleStepWidth: 1,
+  scaleStartValue: 9
+});
 
 //-------------------------------------------
 
@@ -36,7 +49,7 @@ var proData = {
       pointStrokeColor : "#fff",
       pointHighlightFill : "#fff",
       pointHighlightStroke : "rgba(220,220,220,1)",
-      data : [.03,null,.04,null,.02,.07,.03]
+      data : [35,38,45,42,47,39,50]
     }
   ]
 }
@@ -47,8 +60,8 @@ window.myLine = new Chart(ctx, {width: 375, height: 187})
   bezierCurve : false,
   scaleOverride: true,
   scaleSteps: 8,
-  scaleStepWidth: .01,
-  scaleStartValue: .01
+  scaleStepWidth: 6 ,
+  scaleStartValue: 25
 });
 
 //-------------------------------------------
@@ -64,7 +77,7 @@ var b12Data = {
       pointStrokeColor : "#fff",
       pointHighlightFill : "#fff",
       pointHighlightStroke : "rgba(220,220,220,1)",
-      data : [.01,null,null,.07,null,null,.03]
+      data : [180,null,null,212,null,null,230]
     }
   ]
 }
@@ -74,10 +87,43 @@ new Chart(ctx, {width: 375, height: 187})
 .Line(b12Data, {
   bezierCurve : false,
   scaleOverride: true,
-  scaleSteps: 8,
-  scaleStepWidth: .01,
-  scaleStartValue: .01
+  scaleSteps: 12,
+  scaleStepWidth: 30,
+  scaleStartValue: 80
 });
+
+//-------------------------------------------
+
+var b12Data = {
+  labels : ["January","February","March","April","May","June","July"],
+  datasets : [
+    {
+      label: "My First dataset",
+      fillColor : "rgba(0,220,220,0)",
+      strokeColor : "rgba(220,220,220,1)",
+      pointColor : "rgba(0,90,0,1)",
+      pointStrokeColor : "#fff",
+      pointHighlightFill : "#fff",
+      pointHighlightStroke : "rgba(220,220,220,1)",
+      data : [134,131,133,129,130,129,128]
+    }
+  ]
+}
+
+var ctx = $("#canvas-weight")[0].getContext("2d");
+new Chart(ctx, {width: 375, height: 187})
+.Line(b12Data, {
+  scaleShowValues: true,
+  scaleValuePaddingX: 13,
+  scaleValuePaddingY: 13,
+  bezierCurve : false,
+  scaleOverride: true,
+  scaleSteps: 8,
+  scaleStepWidth: 5,
+  scaleStartValue: 110
+});
+
+//-------------------------------------------
 
 $( ".clickme" ).click(function() {
   $(this).find(".canvas-background").animate({
